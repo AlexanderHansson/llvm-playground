60deb5372a069fee8e4d18b5409ddca065ef642a
Alexander Hansson <alexander.hansson4@gmail.com>
2023-03-18 18:32:31 +0100

    Cleanup compile script and move to clang

--- compile.sh
+++ compile.sh
@@ -5,21 +5,25 @@
 mkdir -p build
 mkdir -p install
 
+# cleanup build
+rm -rf build/*
+
 # run generate
-cmake -S llvm-project/llvm -B build -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=./install -DLLVM_ENABLE_PROJECTS="clang" -DCMAKE_BUILD_TYPE=Debug -DLLVM_USE_LINKER=lld
-#cmake -S llvm-project/llvm -B build -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_BUILD_TYPE=Debug
-
-# to run all regression tests
-#cmake --build build check-all
-
-# for sub projects
-#cmake --build build check-project
+cmake -S llvm-project/llvm -B build -G "Unix Makefiles" \
+    -DCMAKE_INSTALL_PREFIX=./install \
+    -DLLVM_ENABLE_PROJECTS="clang" \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DLLVM_USE_LINKER=lld \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_FLAGS="-O3" \
+    -DCMAKE_C_FLAGS="-O3"
 
 # build with all but 2 cores
 cmake --build build -j$(nproc --ignore 2)
 
 # install
 pushd build
-make install
+    make install
 popd
 
